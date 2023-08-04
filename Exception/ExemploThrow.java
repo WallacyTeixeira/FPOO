ff3a67f5dd4e8d3f38441a313d08b921677a1879
Wallacy <Wallacy.teixeira@outlook.com>
2023-08-04 18:46:28 -0300

    03/08/2023

--- Exception/ExemploThrow.java
+++ Exception/ExemploThrow.java
@@ -4,9 +4,31 @@ import javax.swing.JOptionPane;
 
 public class ExemploThrow {
     public static void main(String[] args) {
-        //digitação de senhas
-        String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos");
-        
+        // digitação de senhas e login
+        	
+        boolean testesenha = true;
+        while (testesenha) {
+            String login = JOptionPane.showInputDialog("Informe um Login");
+            String dataNascimento = JOptionPane.showInputDialog("Informe a Data de Nascimento [dd/mm/aa]");
+            dataNascimento = dataNascimento.replace("/", "");
+            String senhaDigitada = JOptionPane.showInputDialog("Informe uma Senha de 6 Digitos");
+            try {
+                if(senhaDigitada.length() != 6) {
+                    throw new Exception("Senha Inválida");
+                }
+                if(login.equals(senhaDigitada)){
+                    throw new Exception("Senha igual Login");
+                }
+                if (senhaDigitada.equals(dataNascimento)) {
+                    throw new Exception("Senha igual Data de Nascimento");
+                }
+
+
+                
+                testesenha=false;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(),null,JOptionPane.CLOSED_OPTION);
+            }
+        }
     }
 }
-
